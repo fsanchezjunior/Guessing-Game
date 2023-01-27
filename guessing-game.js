6cbad4813c3063c166b05681db7bf283c02fd299
Francisco Sanchez Jr <fsjunior10@icloud.com>
2023-01-26 23:00:16 -0800

    Finished the game. There is still some bonus functionality I could add.

--- guessing-game.js
+++ guessing-game.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
 });
 
 
-secretNumber = 5;
+let secretNumber;
  
 function checkGuess(num) {
     if (num > secretNumber) {
@@ -32,4 +32,20 @@ function askGuess() {
     });
 }
 
-askGuess();
+function randomInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function askRange() {
+    rl.question("Enter a min number: ", min => {
+        rl.question("Enter a max number: ", max => {
+            console.log(`I'm thinking of a number between ${min} and ${max}...`);
+            secretNumber = randomInRange(Number(min), Number(max));
+            askGuess();
+        });
+    });
+}
+
+askRange();
